bf1da77490b8160b4d64056210cd69057c7b99d3
LTIFOUR <madjid.tifour.mt@gmail.com>
2022-04-28 09:54:16 +0200

    user optional

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :hashtags
   accepts_nested_attributes_for :hashtags
 
-  belongs_to :user
+  belongs_to :user, optional: true
 
   def repost_count
     # code here

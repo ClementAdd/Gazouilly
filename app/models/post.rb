13e0f0d7770062cf0aa7572e7d74d89b0a54f96f
Maxi-Mega <ixam.agem@gmail.com>
2022-05-04 22:19:50 +0200

    Add author to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :hashtags
   accepts_nested_attributes_for :hashtags
 
-  belongs_to :user, optional: true
+  belongs_to :user, optional: false
 
   def repost_count
     # code here
@@ -12,7 +12,7 @@ class Post < ApplicationRecord
     # code here
   end
 
-  def get_author_name(author_id)
-    User.find(id = author_id).user_name
+  def get_author_name
+    User.find(id = user_id).user_name
   end
 end
--- db/migrate/20220412084212_create_posts.rb
+++ db/migrate/20220412084212_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :text
+      t.integer :user_id, null: false
       t.integer :repost_count
       t.string :repost_id
       t.integer :like_counter

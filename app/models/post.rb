e9e75baceabcf14d0a9169290b12c4f1e5464b87
LTIFOUR <madjid.tifour.mt@gmail.com>
2022-05-04 22:20:56 +0200

    fix relation add some style

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :hashtags
   accepts_nested_attributes_for :hashtags
 
-  belongs_to :user, optional: true
-
   def repost_count
     # code here
   end
@@ -13,6 +11,6 @@ class Post < ApplicationRecord
   end
 
   def get_author_name(author_id)
-    User.find(id = author_id).user_name
+    # User.find(id = author_id).user_name
   end
 end
